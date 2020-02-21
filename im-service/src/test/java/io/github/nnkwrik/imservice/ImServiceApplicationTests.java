e9e043facdf4aef4f7631db2357b2591f41b764b
wywd12315 <wywd12315@gmail.com>
2020-02-20 22:58:39 -0600

    version 0.95a

---
+++ im-service/src/test/java/io/github/nnkwrik/imservice/ImServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.nnkwrik.imservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class ImServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
